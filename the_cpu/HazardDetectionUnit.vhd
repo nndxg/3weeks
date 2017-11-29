f3330a63e6ccc176b51f5a4be2db4e1771df610f
no-more-github <yyljkbgthg2@163.com>
2017-11-30 01:13:59 +0800

    exemem and memwb registers finished

--- the_cpu/HazardDetectionUnit.vhd
+++ the_cpu/HazardDetectionUnit.vhd
@@ -30,12 +30,33 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity HazardDetectionUnit is
+	port(
+		IdExeMemRead: in std_logic;
+		IdExeWriteReg: in std_logic_vector(3 downto 0);
+		readReg1: in std_logic_vector(3 downto 0);
+		readReg2: in std_logic_vector(3 downto 0);
+		
+		IdExeFlush_LW: out std_logic;
+		PCKeep: out std_logic;
+		IfIdFlush_LW: out std_logic
+	);
 end HazardDetectionUnit;
 
 architecture Behavioral of HazardDetectionUnit is
 
 begin
 
+	process(IdExeMemRead, IeExeWriteReg, readReg1, readReg2)
+	begin
+		if ((IdExeMemRead = '1') and ((readReg1 = IdExeWriteReg) or (readReg2 = IdExeWriteReg))) then
+			IdExeFlush_LW <= '1';
+			PCKeep <= '1';
+			IfIdFlush_LW <= '1';
+		else
+			IdExeFlush_LW <= '0';
+			PCKeep <= '0';
+			IfIdFlush_LW <= '0';
+		end if;
+	end process;
 
-end Behavioral;
-
+end Behavioral 
---
+++ the_cpu/MemWbRegisters.vhd
@@ -0,0 +1,70 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    23:40:07 11/29/2017 
+-- Design Name: 
+-- Module Name:    MemWbRegisters - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity MemWbRegisters is
+	port(
+		rst: in std_logic;
+		clk: in std_logic;
+		ExeMemRegWrite: in std_logic;
+		ExeMemWBSrc: in std_logic;
+		MemReadData: in std_logic_vector(15 downto 0);
+		ALUResult: in std_logic_vector(15 downto 0);
+		ExeMemWriteReg: in std_logic_vector(15 downto 0);
+		
+		MemWbRegWrite: out std_logic;
+		MemWbWriteReg: out std_logic_vector(15 downto 0);
+		WriteData: out std_logic_vector(15 downto 0)
+	);
+end MemWbRegisters;
+
+architecture Behavioral of MemWbRegisters is
+
+begin
+
+	process(rst, clk)
+	begin 
+		if (rst = '0') then
+			MemWbRegWrite <= '0';
+			MemWbWriteReg <= "1110";   -- ±íÊ¾²»Ð´¼Ä´æÆ÷
+			WriteData <= (others => '0');
+		elsif (rising_edge(clk)) then
+			MemWbRegWrite <= ExeMemRegWrite;
+			MemWbWriteReg <= ExeMemWriteReg;
+			if (ExeMemWBSrc = '1') then
+				WriteData <= MemReadData;
+			else
+				WriteData <= ALUResult;
+			end if;
+		end if;
+	end process;
+
+end Behavioral;
+
